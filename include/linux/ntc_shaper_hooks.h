f99ea5f1d2d2e4c8991ea4e5899558074c756db0
Sergey Korolev <s.korolev@ndmsystems.com>
2016-03-31 18:00:37 +0300

    NTC shaper hooks added

---
+++ include/linux/ntc_shaper_hooks.h
@@ -0,0 +1,62 @@
+#ifndef _LINUX_NTC_SHAPER_HOOKS_H
+#define _LINUX_NTC_SHAPER_HOOKS_H
+
+#include <linux/spinlock.h>
+
+struct sk_buff;
+
+typedef
+unsigned int ntc_shaper_hook_fn(struct sk_buff *skb,
+				const uint32_t saddr,
+				const uint32_t daddr,
+				uint32_t *disable_hwacc,
+				int (*okfn_nf)(struct sk_buff *),
+				int (*okfn_custom)(struct sk_buff *, void *),
+				void *data);
+
+extern int (*ntc_shaper_check_ip_and_mac)(uint32_t ipaddr, uint8_t *mac);
+
+extern rwlock_t ntc_shaper_lock;
+extern ntc_shaper_hook_fn *ntc_shaper_ingress_hook;
+extern ntc_shaper_hook_fn *ntc_shaper_egress_hook;
+
+static inline ntc_shaper_hook_fn *
+ntc_shaper_ingress_hook_get(void)
+{
+	read_lock_bh(&ntc_shaper_lock);
+
+	return ntc_shaper_ingress_hook;
+}
+
+static inline void
+ntc_shaper_ingress_hook_put(void)
+{
+	read_unlock_bh(&ntc_shaper_lock);
+}
+
+static inline ntc_shaper_hook_fn *
+ntc_shaper_egress_hook_get(void)
+{
+	read_lock_bh(&ntc_shaper_lock);
+
+	return ntc_shaper_egress_hook;
+}
+
+static inline void
+ntc_shaper_egress_hook_put(void)
+{
+	read_unlock_bh(&ntc_shaper_lock);
+}
+
+static inline void
+ntc_shaper_hooks_set(ntc_shaper_hook_fn *ingress_hook,
+		     ntc_shaper_hook_fn *egress_hook)
+{
+	write_lock_bh(&ntc_shaper_lock);
+	ntc_shaper_ingress_hook = ingress_hook;
+	ntc_shaper_egress_hook = egress_hook;
+	write_unlock_bh(&ntc_shaper_lock);
+}
+
+#endif
+
---
+++ net/core/ntc_shaper_hooks.c
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/ntc_shaper_hooks.h>
+
+rwlock_t ntc_shaper_lock = __RW_LOCK_UNLOCKED(ntc_shaper_lock);
+EXPORT_SYMBOL(ntc_shaper_lock);
+
+ntc_shaper_hook_fn *ntc_shaper_ingress_hook = NULL;
+EXPORT_SYMBOL(ntc_shaper_ingress_hook);
+
+ntc_shaper_hook_fn *ntc_shaper_egress_hook = NULL;
+EXPORT_SYMBOL(ntc_shaper_egress_hook);
+
+int (*ntc_shaper_check_ip_and_mac)(uint32_t ipaddr, uint8_t * mac) = NULL;
+EXPORT_SYMBOL(ntc_shaper_check_ip_and_mac);
+
