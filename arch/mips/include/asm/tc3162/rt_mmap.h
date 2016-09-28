4772ae28c994655fe38eee54e95c4306f58c7c49
Andrey Zolotarev <a.zolotarev@ndmsystems.com>
2016-09-29 00:44:59 +0700

    arch: tc3262: add rt_mmap.h, surfboardint.h for build external modules such as vport

---
+++ arch/mips/include/asm/tc3162/rt_mmap.h
@@ -0,0 +1,92 @@
+/**************************************************************************
+ *
+ *  BRIEF MODULE DESCRIPTION
+ *     register definition for Ralink RT-series SoC
+ *
+ *  Copyright 2007 Ralink Inc.
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ *
+ *  THIS  SOFTWARE  IS PROVIDED   ``AS  IS'' AND   ANY  EXPRESS OR IMPLIED
+ *  WARRANTIES,   INCLUDING, BUT NOT  LIMITED  TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
+ *  NO  EVENT  SHALL   THE AUTHOR  BE    LIABLE FOR ANY   DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED   TO, PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES; LOSS OF
+ *  USE, DATA,  OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  You should have received a copy of the  GNU General Public License along
+ *  with this program; if not, write  to the Free Software Foundation, Inc.,
+ *  675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *
+ **************************************************************************
+ */
+
+#ifndef __ASM_MACH_MIPS_TC3262_RT_MMAP_H
+#define __ASM_MACH_MIPS_TC3262_RT_MMAP_H
+
+#define RALINK_SYSCTL_BASE		0xBFB00000
+#define RALINK_TIMER_BASE		0xBFBF0100
+#define RALINK_INTCL_BASE		0xBFB40000
+#define RALINK_MEMCTRL_BASE		0xBFB20000
+#define RALINK_PIO_BASE			0xBFBF0200
+#define RALINK_NAND_CTRL_BASE		0xBFBE0010
+#define RALINK_NANDECC_CTRL_BASE	0xBFBE0040
+#define RALINK_I2C_BASE			0xBFBF8000
+#define RALINK_I2S_BASE			0xBFBF8100
+#define RALINK_SPI_BASE			0xBFBC0B00
+#define RALINK_UART_LITE_BASE		0xBFBF0000
+#define RALINK_UART_LITE2_BASE		0xBFBF0300
+#define RALINK_PCM_BASE			0xBFBD0000
+#define RALINK_GDMA_BASE		0xBFB30000
+#define RALINK_FRAME_ENGINE_BASE	0xBFB50000
+#define RALINK_ETH_SW_BASE		0xBFB58000
+#define RALINK_CRYPTO_ENGINE_BASE	0xBFB70000
+#define RALINK_PCI_BASE			0xBFB80000
+#define RALINK_USB_HOST_BASE		0x1FBB0000
+#define RALINK_11N_MAC_BASE		0xBFB00000 // Unused
+
+//Interrupt Controller
+#define RALINK_INTCTL_UARTLITE		(1<<0)
+#define RALINK_INTCTL_PIO		(1<<10)
+#define RALINK_INTCTL_PCM		(1<<11)
+#define RALINK_INTCTL_DMA		(1<<14)
+#define RALINK_INTCTL_GMAC2		(1<<15)
+#define RALINK_INTCTL_PCI		(1<<17)
+#define RALINK_INTCTL_UHST2		(1<<20)
+#define RALINK_INTCTL_GMAC1		(1<<21)
+#define RALINK_INTCTL_UHST1		(1<<23)
+#define RALINK_INTCTL_PCIE		(1<<24)
+#define RALINK_INTCTL_NAND		(1<<25)
+#define RALINK_INTCTL_SPI		(1<<27)
+
+//Reset Control Register
+#define RALINK_CRYPTO_RST		(1<<6)
+#define RALINK_TIMER_RST		(1<<8)
+#define RALINK_INTC_RST			(1<<9)
+#define RALINK_MC_RST			(1<<10)
+#define RALINK_PCM_RST			(1<<11)
+#define RALINK_UART_RST			(1<<12)
+#define RALINK_PIO_RST			(1<<13)
+#define RALINK_DMA_RST			(1<<14)
+#define RALINK_NAND_RST			(1<<15)
+#define RALINK_I2C_RST			(1<<16)
+#define RALINK_I2S_RST			(1<<17)
+#define RALINK_SPI_RST			(1<<18)
+#define RALINK_UARTL_RST		(1<<19)
+#define RALINK_FE_RST			(1<<21)
+#define RALINK_UHST_RST			(1<<22)
+#define RALINK_ESW_RST			(1<<23)
+#define RALINK_EPHY_RST			(1<<24)
+#define RALINK_UDEV_RST			(1<<25)
+#define RALINK_PCIE0_RST		(1<<26)
+#define RALINK_PCIE1_RST		(1<<27)
+
+#endif
---
+++ arch/mips/include/asm/tc3162/surfboardint.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2001 Palmchip Corporation.  All rights reserved.
+ *
+ * ########################################################################
+ *
+ *  This program is free software; you can distribute it and/or modify it
+ *  under the terms of the GNU General Public License (Version 2) as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ * ########################################################################
+ *
+ * Defines for the Surfboard interrupt controller.
+ *
+ */
+#ifndef __ASM_MACH_MIPS_TC3262_SURFBOARDINT_H
+#define __ASM_MACH_MIPS_TC3262_SURFBOARDINT_H
+
+#define SURFBOARDINT_GPIO		11	/* GPIO */
+#define SURFBOARDINT_DMA		15	/* DMA */
+#define SURFBOARDINT_I2S 		35	/* I2S */
+#define SURFBOARDINT_ESW		16	/* ESW */
+#define SURFBOARDINT_UHST		18	/* USB Host */
+#define SURFBOARDINT_PCM		12	/* PCM */
+#define SURFBOARDINT_FE			22	/* Frame Engine */
+#define SURFBOARDINT_PCIE0		25	/* PCIe0 */
+#define SURFBOARDINT_PCIE1		24	/* PCIe1 */
+#define SURFBOARDINT_CRYPTO		29	/* CryptoEngine */
+
+extern void tc_enable_irq(unsigned int irq);
+extern void tc_disable_irq(unsigned int irq);
+
+#endif
