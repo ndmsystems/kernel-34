4411af4e83e55a514b67c6c338b5cc71b61251c8
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2017-01-10 14:57:14 +0300

    include/net/fast_vpn: add SWNAT_KA_SET_MARK/SWNAT_KA_CHECK_MARK macro (case 9415)

--- include/net/fast_vpn.h
+++ include/net/fast_vpn.h
@@ -25,6 +25,7 @@
 
 #define SWNAT_FNAT_MARK		0x01
 #define SWNAT_PPP_MARK		0x02
+#define SWNAT_KA_MARK		0x04
 
 /* FNAT mark */
 
@@ -59,6 +60,18 @@ do { \
 } while (0);
 
 /* End of PPP mark */
+
+/* KA mark */
+
+#define SWNAT_KA_SET_MARK(skb_) \
+do { \
+	(skb_)->cb[SWNAT_CB_OFFSET] = SWNAT_KA_MARK; \
+} while (0);
+
+#define SWNAT_KA_CHECK_MARK(skb_) \
+	((skb_)->cb[SWNAT_CB_OFFSET] == SWNAT_KA_MARK)
+
+/* End of KA mark */
 
 /* List of new MC streams */
 
