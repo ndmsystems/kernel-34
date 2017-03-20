3e5ae9f71ce2ddd74083dfb4daddfe651d2759f6
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2017-03-20 17:40:32 +0300

    net/netfilter/nf_fp_smb: add support for multiple segments (case 8937)

--- net/netfilter/nf_fp_smb.c
+++ net/netfilter/nf_fp_smb.c
@@ -11,22 +11,42 @@
 
 #include <net/netfilter/nf_fp_smb.h>
 
-static __be32 nf_fp_smb_host __read_mostly = 0;
+#define HOSTS_COUNT		8
+#define PHOST_AT(idx_)	((__be32 *)nf_fp_smb_hosts + (idx_))
+#define HOST_AT(idx_)	(*(PHOST_AT(idx_)))
+
+static u32 nf_fp_smb_on __read_mostly = 0;
+static __be32 nf_fp_smb_hosts[HOSTS_COUNT] __read_mostly;
 
 int nf_fp_smb_hook_in(struct sk_buff *skb)
 {
 	const struct iphdr *iph = ip_hdr(skb);
 	const struct tcphdr *tcph;
 	struct tcphdr _hdr;
+	u32 found = 0;
+	size_t i;
 
-	if (!nf_fp_smb_host)
+	if (!nf_fp_smb_on)
 		return 0;
 
 	if (unlikely(iph->protocol != IPPROTO_TCP))
 		return 0;
 
-	if (likely(iph->daddr != nf_fp_smb_host))
+	for (i = 0; i < HOSTS_COUNT; ++i) {
+		if (iph->daddr == HOST_AT(i)) {
+			found = 1;
+
+			break;
+		}
+
+		if (HOST_AT(i) == 0) {
+			break;
+		}
+	}
+
+	if (likely(!found)) {
 		return 0;
+	}
 
 	tcph = skb_header_pointer(skb, (iph->ihl<<2), sizeof(_hdr), &_hdr);
 	if (!tcph)
@@ -46,15 +66,30 @@ int nf_fp_smb_hook_out(struct sk_buff *skb)
 	const struct iphdr *iph = ip_hdr(skb);
 	const struct tcphdr *tcph;
 	struct tcphdr _hdr;
+	u32 found = 0;
+	size_t i;
 
-	if (!nf_fp_smb_host)
+	if (!nf_fp_smb_on)
 		return 0;
 
 	if (unlikely(iph->protocol != IPPROTO_TCP))
 		return 0;
 
-	if (likely(iph->saddr != nf_fp_smb_host))
+	for (i = 0; i < HOSTS_COUNT; ++i) {
+		if (iph->saddr == HOST_AT(i)) {
+			found = 1;
+
+			break;
+		}
+
+		if (HOST_AT(i) == 0) {
+			break;
+		}
+	}
+
+	if (likely(!found)) {
 		return 0;
+	}
 
 	tcph = skb_header_pointer(skb, (iph->ihl<<2), sizeof(_hdr), &_hdr);
 	if (!tcph)
@@ -70,16 +105,29 @@ int nf_fp_smb_hook_out(struct sk_buff *skb)
 }
 
 #ifdef CONFIG_PROC_FS
+
 static int nf_fp_smb_seq_show(struct seq_file *s, void *v)
 {
-	return seq_printf(s, "%pI4\n", &nf_fp_smb_host);
+	size_t i;
+
+	for (i = 0; i < HOSTS_COUNT; ++i) {
+		seq_printf(s, "%pI4\n", PHOST_AT(i));
+	}
+
+	return 0;
 }
 
-static ssize_t nf_fp_smb_seq_write(struct file *file, const char __user *buffer, size_t count, loff_t *ppos)
+static int nf_fp_smb_off_seq_show(struct seq_file *s, void *v)
+{
+	return seq_printf(s, "%u\n", nf_fp_smb_on);
+}
+
+static ssize_t nf_fp_smb_seq_write(struct file *file,
+	const char __user *buffer, size_t count, loff_t *ppos)
 {
 	char buf[20];
 	u32 in[4], smb_ip = 0;
-	int i;
+	size_t i;
 
 	if (count > (sizeof(buf) - 1))
 		count = (sizeof(buf) - 1);
@@ -101,7 +149,30 @@ static ssize_t nf_fp_smb_seq_write(struct file *file, const char __user *buffer,
 	if (smb_ip == (u32)-1)
 		return count;
 
-	nf_fp_smb_host = htonl(smb_ip);
+	for (i = 0; i < HOSTS_COUNT; ++i) {
+		if (HOST_AT(i) == 0 || HOST_AT(i) == cpu_to_be32(smb_ip)) {
+			HOST_AT(i) = cpu_to_be32(smb_ip);
+
+			printk(KERN_INFO "enable SMB fastpath for %pI4\n", PHOST_AT(i));
+
+			break;
+		}
+	}
+
+	return count;
+}
+
+static ssize_t nf_fp_smb_off_seq_write(struct file *file,
+	const char __user *buffer, size_t count, loff_t *ppos)
+{
+	if (count == 0)
+		return count;
+
+	nf_fp_smb_on = 0;
+
+	memset(nf_fp_smb_hosts, 0, HOSTS_COUNT * sizeof(__be32));
+
+	printk(KERN_INFO "disable SMB fastpath\n");
 
 	return count;
 }
@@ -128,9 +199,21 @@ static const struct seq_operations nf_fp_smb_seq_ops = {
 	.show	= nf_fp_smb_seq_show,
 };
 
+static const struct seq_operations nf_fp_smb_off_seq_ops = {
+	.start	= seq_start,
+	.next	= seq_next,
+	.stop	= seq_stop,
+	.show	= nf_fp_smb_off_seq_show,
+};
+
 static int nf_fp_smb_seq_open(struct inode *inode, struct file *file)
 {
 	return seq_open(file, &nf_fp_smb_seq_ops);
+}
+
+static int nf_fp_smb_off_seq_open(struct inode *inode, struct file *file)
+{
+	return seq_open(file, &nf_fp_smb_off_seq_ops);
 }
 
 static const struct file_operations nf_fp_smb_fops = {
@@ -141,12 +224,25 @@ static const struct file_operations nf_fp_smb_fops = {
 	.release	= seq_release,
 };
 
+static const struct file_operations nf_fp_smb_off_fops = {
+	.open		= nf_fp_smb_off_seq_open,
+	.write		= nf_fp_smb_off_seq_write,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= seq_release,
+};
+
 #endif
 
 int __init netfilter_fp_smb_init(void)
 {
+	memset(nf_fp_smb_hosts, 0, HOSTS_COUNT * sizeof(__be32));
+
 #ifdef CONFIG_PROC_FS
 	if (!proc_create("nf_fp_smb", 0664, proc_net_netfilter, &nf_fp_smb_fops))
+		return -ENOMEM;
+	if (!proc_create("nf_fp_smb_off", 0664,
+			proc_net_netfilter, &nf_fp_smb_off_fops))
 		return -ENOMEM;
 #endif
 
