d1fb9c80be9369d136c0fa7cbb126dc3be1df630
Igor Mokrushin <i.mokrushin@ndmsystems.com>
2017-09-07 20:19:00 +0300

    Adding symbols export for work xDSL drivers.

--- net/atm/br2684.c
+++ net/atm/br2684.c
@@ -274,7 +274,7 @@ static inline struct br2684_vcc *pick_outgoing_vcc(const struct sk_buff *skb,
 	return list_empty(&brdev->brvccs) ? NULL : list_entry_brvcc(brdev->brvccs.next);	/* 1 vcc/dev right now */
 }
 
-static netdev_tx_t br2684_start_xmit(struct sk_buff *skb,
+netdev_tx_t br2684_start_xmit(struct sk_buff *skb,
 				     struct net_device *dev)
 {
 	struct br2684_dev *brdev = BRPRIV(dev);
@@ -306,6 +306,7 @@ static netdev_tx_t br2684_start_xmit(struct sk_buff *skb,
 	read_unlock(&devs_lock);
 	return NETDEV_TX_OK;
 }
+EXPORT_SYMBOL(br2684_start_xmit);
 
 /*
  * We remember when the MAC gets set, so we don't override it later with
