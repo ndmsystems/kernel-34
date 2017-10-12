e819aa8f1d70293f6c8b715a4ec182ffd0ac1c00
Andrey Zolotarev <a.zolotarev@ndmsystems.com>
2017-10-12 16:44:05 +0700

    net/core: add new igmpsn hook to faster processing, case 11201

--- net/core/ndm_hooks.c
+++ net/core/ndm_hooks.c
@@ -72,6 +72,9 @@ EXPORT_SYMBOL(prebind_from_mc_output);
 void (*update_mc_streams)(struct new_mc_streams * streams_list) = NULL;
 EXPORT_SYMBOL(update_mc_streams);
 
+int (*igmpsn_hook)(struct sk_buff *skb) = NULL;
+EXPORT_SYMBOL(igmpsn_hook);
+
 int (*pppoe_pthrough)(struct sk_buff *skb) = NULL;
 EXPORT_SYMBOL(pppoe_pthrough);
 
