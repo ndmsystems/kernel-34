62ed7a876efd101c92c31b6553522544fcbc8289
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2020-08-07 22:16:45 +0300

    ch341.c: sync with kernel 4.9

--- drivers/usb/serial/ch341.c
+++ drivers/usb/serial/ch341.c
@@ -74,6 +74,7 @@ static bool debug;
 
 static const struct usb_device_id id_table[] = {
 	{ USB_DEVICE(0x4348, 0x5523) },
+	{ USB_DEVICE(0x1a86, 0x7522) },
 	{ USB_DEVICE(0x1a86, 0x7523) },
 	{ USB_DEVICE(0x1a86, 0x5523) },
 	{ },
